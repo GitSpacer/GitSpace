720b1052742a1f80b2daad8e36a84fee6f2eab79
dahae0320 <ldh990320ldh@gmail.com>
2023-10-12 19:48:50 +0900

    [Feat] #5 hex값 적용을 위한 임시 Color extension 추가 - merge 후 중복될 경우 삭제 바람

--- GitSpace/GSDesignSystem/GSCanvas.swift
+++ GitSpace/GSDesignSystem/GSCanvas.swift
@@ -133,3 +133,27 @@ extension GSCanvas {
         .padding(.horizontal)
     }
 }
+
+// MARK: Hexa Color (Merge 후 같은 파일로 통일할 것)
+extension Color {
+    init(hex: Int, opacity: Double = 1.0) {
+        let red = Double((hex >> 16) & 0xff) / 255
+        let green = Double((hex >> 8) & 0xff) / 255
+        let blue = Double((hex >> 0) & 0xff) / 255
+
+        self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
+    }
+
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#")
+
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+
+        let r = Double((rgb >> 16) & 0xFF) / 255.0
+        let g = Double((rgb >>  8) & 0xFF) / 255.0
+        let b = Double((rgb >>  0) & 0xFF) / 255.0
+        self.init(red: r, green: g, blue: b)
+    }
+}
