4fc9f7ca80adc54f7681db0ac9120d0f5f0c7f87
jegyun <parkjekyun@gmail.com>
2023-10-12 16:09:17 +0900

    [Add] GSText 파일 생성

---
+++ GitSpace/GSDesignSystem/GSText.swift
@@ -0,0 +1,21 @@
+//
+//  GSText.swift
+//  GSDesignSystem
+//
+//  Created by 박제균 on 10/12/23.
+//
+
+import SwiftUI
+
+struct GSText: View {
+
+  
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GSText()
+}
