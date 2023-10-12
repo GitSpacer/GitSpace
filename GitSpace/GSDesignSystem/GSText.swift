96eb1903bf63bad29f93365d1530fa48e414d7ab
jegyun <parkjekyun@gmail.com>
2023-10-12 17:20:18 +0900

    [Feat] style 별 body 구현 및 GSTextConstants 열거형 추가
    body를 style에 따라 구현하였습니다. 이에 따라 필요한 상수값들을 선언한
    GSTextConstants 열거형을 추가하였습니다.
    
    Ref: #7

--- GitSpace/GSDesignSystem/GSText.swift
+++ GitSpace/GSDesignSystem/GSText.swift
@@ -25,8 +25,94 @@ public struct GSText: View {
     }
 
     public var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        switch style {
+        case .title1:
+            Text(.init(string))
+                .font(.system(size: GSTextConstants.title1Size))
+                .fontWeight(GSTextConstants.title1FontWeight)
+                .foregroundStyle(GSTextConstants.title1Color)
+        case .title2:
+            Text(.init(string))
+                .font(.system(size: GSTextConstants.title2Size))
+                .fontWeight(GSTextConstants.title2FontWeight)
+                .foregroundStyle(GSTextConstants.title2Color)
+        case .title3:
+            Text(.init(string))
+                .font(.system(size: GSTextConstants.title3Size))
+                .fontWeight(GSTextConstants.title3FontWeight)
+                .foregroundStyle(GSTextConstants.title3Color)
+        case .sectionTitle:
+            Text(.init(string))
+                .font(.system(size: GSTextConstants.sectionTitleSize))
+                .fontWeight(GSTextConstants.sectionTitleFontWeight)
+                .foregroundStyle(GSTextConstants.sectionTitleColor)
+        case .body1:
+            Text(.init(string))
+                .font(.system(size: GSTextConstants.body1Size))
+                .fontWeight(GSTextConstants.body1FontWeight)
+                .foregroundStyle(GSTextConstants.body1Color)
+        case .body2:
+            Text(.init(string))
+                .font(.system(size: GSTextConstants.body2Size))
+                .fontWeight(GSTextConstants.body2FontWeight)
+                .foregroundStyle(GSTextConstants.body2Color)
+        case .caption1:
+            Text(.init(string))
+                .font(.system(size: GSTextConstants.caption1Size))
+                .fontWeight(GSTextConstants.caption1FontWeight)
+                .foregroundStyle(GSTextConstants.caption1Color)
+        case .caption2:
+            Text(.init(string))
+                .font(.system(size: GSTextConstants.caption2Size))
+                .fontWeight(GSTextConstants.caption2FontWeight)
+                .foregroundStyle(GSTextConstants.caption2Color)
+        case .description:
+            Text(.init(string))
+                .font(.system(size: GSTextConstants.descriptionSize))
+                .fontWeight(GSTextConstants.descriptionFontWeight)
+                .foregroundStyle(GSTextConstants.descriptionColor)
+        }
     }
+}
+
+enum GSTextConstants {
+    static let title1Size: CGFloat = 28
+    static let title1FontWeight: Font.Weight = .semibold
+    static let title1Color: Color = Color.primary
+
+    static let title2Size: CGFloat = 20
+    static let title2FontWeight: Font.Weight = .semibold
+    static let title2Color: Color = Color.primary
+
+    static let title3Size: CGFloat = 16
+    static let title3FontWeight: Font.Weight = .semibold
+    static let title3Color: Color = Color.primary
+
+    static let sectionTitleSize: CGFloat = 13
+    static let sectionTitleFontWeight: Font.Weight = .regular
+    static let sectionTitleColor: Color = Color.gsGray2
+
+    static let body1Size: CGFloat = 16
+    static let body1FontWeight: Font.Weight = .regular
+    static let body1Color: Color = Color.primary
+
+    static let body2Size: CGFloat = 12
+    static let body2FontWeight: Font.Weight = .regular
+    static let body2Color: Color = Color.gsGray1
+
+    static let caption1Size: CGFloat = 13
+    static let caption1FontWeight: Font.Weight = .medium
+    static let caption1Color: Color = Color.gsGray2
+    static let caption1PrimaryColor: Color = Color.primary
+
+    static let caption2Size: CGFloat = 12
+    static let caption2FontWeight: Font.Weight = .regular
+    static let caption2Color: Color = Color.gsGray2
+    static let caption2PrimaryColor: Color = Color.primary
+
+    static let descriptionSize: CGFloat = 16
+    static let descriptionFontWeight: Font.Weight = .medium
+    static let descriptionColor: Color = Color.gsGray2
 }
 
 #Preview {
