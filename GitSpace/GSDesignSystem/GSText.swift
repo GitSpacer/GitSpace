23de56375885a6aab2a5c34c091deca49e557acd
jegyun <parkjekyun@gmail.com>
2023-10-12 16:55:20 +0900

    [Feat] style, string 프로퍼티 추가 및 이니셜라이저 정의
    스타일 지정을 위한 style 프로퍼티와 입력받을 문자열을 위한 string
    프로퍼티를 추가하였습니다.
    
    또한 GSText 사용을 위한 이니셜라이저를 정의하였습니다.
    
    Ref: #7

--- GitSpace/GSDesignSystem/GSText.swift
+++ GitSpace/GSDesignSystem/GSText.swift
@@ -9,11 +9,19 @@ import SwiftUI
 
 public struct GSText: View {
 
+    let style: GSTextStyle
+    let string: String
+
     public enum GSTextStyle {
         case title1, title2, title3, sectionTitle
         case body1, body2
         case caption1, caption2
         case description
+    }
+
+    init(style: GSTextStyle, string: String) {
+        self.style = style
+        self.string = string
     }
 
     public var body: some View {
