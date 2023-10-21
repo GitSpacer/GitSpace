bbc369abd57ca17515ba47840942658d83c04b77
valselee <sollleky72@gmail.com>
2023-10-21 22:57:05 +0900

    [Add/#2] 1. GSTextField 파일 생성

---
+++ GitSpace/GSDesignSystem/GSTextFiedl.swift
@@ -0,0 +1,18 @@
+//
+//  GSTextFiedl.swift
+//  GSDesignSystem
+//
+//  Created by Celan on 10/21/23.
+//
+
+import SwiftUI
+
+struct GSTextFiedl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GSTextFiedl()
+}
---
+++ GitSpace/GSDesignSystem/GSTextField.swift
@@ -0,0 +1,18 @@
+//
+//  GSTextField.swift
+//  GSDesignSystem
+//
+//  Created by Celan on 10/21/23.
+//
+
+import SwiftUI
+
+struct GSTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GSTextField()
+}
