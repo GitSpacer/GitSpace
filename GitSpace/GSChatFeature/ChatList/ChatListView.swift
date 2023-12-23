a2f7eb8d7cce989379cddfc4189a23fde736d741
valselee <sollleky72@gmail.com>
2023-12-23 15:47:48 +0900

    [Add/#16] 1. GSChatFeature의 ChatList Lofi View 디렉토리 생성 2. GSGray3 의 잘못된 hex 수정

---
+++ GitSpace/GSChatFeature/ChatList/ChatListView.swift
@@ -0,0 +1,8 @@
+//
+//  ChatListView.swift
+//  GSChatFeature
+//
+//  Created by Celan on 12/23/23.
+//
+
+import Foundation
---
+++ GitSpace/GSChatFeature/GSChatFeature.h
@@ -0,0 +1,18 @@
+//
+//  GSChatFeature.h
+//  GSChatFeature
+//
+//  Created by Celan on 12/23/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GSChatFeature.
+FOUNDATION_EXPORT double GSChatFeatureVersionNumber;
+
+//! Project version string for GSChatFeature.
+FOUNDATION_EXPORT const unsigned char GSChatFeatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GSChatFeature/PublicHeader.h>
+
+
