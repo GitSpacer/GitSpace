a104378a5302b2a0a5c42ea79516333da170615e
valselee <sollleky72@gmail.com>
2023-12-29 14:50:02 +0900

    [Feat/#16] 1. ChatListView의 lofi 형태 구성 완료 2. ChatDetailView의 lofi 형태 구성 완료 3. 추후 반복되는 컴포넌트를 분리하여 재활용성 향상 필요

--- GitSpace/GSChatFeature/ChatUI/ChatDetailView.swift
+++ GitSpace/GSChatFeature/ChatUI/ChatDetailView.swift
@@ -5,14 +5,181 @@
 //  Created by Celan on 12/28/23.
 //
 
+import GSDesignSystem
 import SwiftUI
 
 struct ChatDetailView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  private func isUsersMessage(_ int: Int) -> Bool {
+    int % 2 == 0
+  }
+  
+  var body: some View {
+    VStack {
+      // MARK: Profile Header Section
+      VStack(spacing: 14) {
+        Image(systemName: "person.circle.fill")
+          .resizable()
+          .frame(width: 100, height: 100)
+        
+        VStack(spacing: 4) {
+          GSText(style: .title3, string: "RandomBrazilGuy")
+          GSText(style: .caption1, string: "@RandomBrazilGuy")
+        }
+        
+        VStack(spacing: 4) {
+          GSText(style: .title3, string: "Starred: ")
+          GSText(style: .caption1, string: "Owner Of: ")
+        }
+      }
+      
+      Divider()
+        .padding(.vertical, 20)
+        .padding(.horizontal, 20)
+      
+      // MARK: LOFI Chat ScrollView
+      ScrollViewReader { proxy in
+        ScrollView {
+          GSText(style: .caption1, string: "22/10/12 14:00 KST")
+            .padding(.bottom, 8)
+          
+          Section {
+            GSText(style: .description, string: "Hi! This is Gildong from South Korea who’s currently studying Web programming. Would you mind giving me some time and advising me on my future career path? Thank you so much for your help!")
+              .padding(.horizontal, 25)
+              .padding(.vertical, 20)
+              .background {
+                RoundedRectangle(cornerRadius: 20)
+                  .foregroundStyle(Color.white)
+                  .shadow(
+                    color: Color.gray.opacity(0.3),
+                    radius: 5,
+                    y: 2
+                  )
+              }
+              .padding(.bottom, 12)
+            
+          } header: {
+            GSText(
+              style: .body2,
+              string: "Knock Message"
+            )
+            .frame(
+              maxWidth: .infinity,
+              alignment: .leading
+            )
+          }
+          .padding(.horizontal, 20)
+          
+          // MARK: Each Chat Bubble
+          ForEach(0..<10) { int in
+            HStack(
+              alignment: .bottom,
+              spacing: 8
+            ) {
+              if isUsersMessage(int) {
+                Text(.now, style: .time)
+                  .font(.footnote)
+                  .foregroundStyle(Color.gsGray1)
+              }
+              
+              if !isUsersMessage(int) {
+                Image(systemName: "person.circle.fill")
+                  .resizable()
+                  .frame(width: 25, height: 25)
+                  .padding(.leading, 20)
+              }
+              
+              GSText(
+                style: .body1,
+                string: "Lorem Ipsum is simply dummy text of the printin Lorem Ipsum Lorem"
+              )
+              .frame(maxWidth: 250)
+              .padding(.horizontal, 12)
+              .padding(.vertical, 18)
+              .background {
+                BubbleBackground(isUsersMessage: isUsersMessage(int))
+                  .foregroundStyle(Color.yellow)
+              }
+              .padding(
+                isUsersMessage(int) ? .trailing : .leading,
+                isUsersMessage(int) ? 20 : 0
+              )
+              .frame(
+                alignment: isUsersMessage(int) ? .trailing : .leading
+              )
+              
+              if !isUsersMessage(int) {
+                Text(.now, style: .time)
+                  .font(.footnote)
+                  .foregroundStyle(Color.gsGray1)
+              }
+            }
+            .frame(
+              maxWidth: .infinity,
+              alignment: isUsersMessage(int) ? .trailing : .leading
+            )
+            .padding(.bottom, 12)
+            .id(int)
+          }
+          .onAppear {
+            #warning("Replace into Chat Documents count")
+            proxy.scrollTo(9)
+          }
+        }
+      }
     }
+    .frame(
+      maxWidth: .infinity,
+      maxHeight: .infinity,
+      alignment: .top
+    )
+    .padding(.top, 20)
+    .navigationBarBackButtonHidden()
+    .toolbar {
+      ToolbarItem(placement: .topBarTrailing) {
+        Button {
+          
+        } label: {
+          Image(systemName: "ellipsis")
+            .foregroundStyle(Color.gsGray2)
+        }
+      }
+      
+      ToolbarItem(placement: .principal) {
+        HStack {
+          Image(systemName: "person.circle.fill")
+            .resizable()
+            .frame(width: 25, height: 25)
+          
+          Text("RandomBrazilGuy")
+        }
+      }
+    }
+  }
 }
 
 #Preview {
-    ChatDetailView()
+  NavigationStack {
+    ChatListView()
+//    ChatDetailView()
+  }
+}
+
+struct BubbleBackground: Shape {
+  let isUsersMessage: Bool
+  
+  func path(in rect: CGRect) -> Path {
+    let path = UIBezierPath(
+      roundedRect: rect,
+      byRoundingCorners:
+        isUsersMessage
+      ? [.topLeft, .bottomLeft, .bottomRight]
+      : [.topRight, .bottomRight, .bottomLeft],
+      cornerRadii: CGSize(
+        width: 20,
+        height: 20
+      )
+    )
+    
+    return Path(path.cgPath)
+  }
 }
--- GitSpace/GSChatFeature/ChatUI/ChatListView.swift
+++ GitSpace/GSChatFeature/ChatUI/ChatListView.swift
@@ -11,166 +11,175 @@ import GSUtilities
 import SwiftUI
 
 struct ChatListView: View {
-    var body: some View {
-        VStack(alignment: .leading) {
-            Section {
-                ScrollView(
-                    .horizontal,
-                    showsIndicators: false
-                ) {
+  var body: some View {
+    VStack(alignment: .leading) {
+      Section {
+        ScrollView(
+          .horizontal,
+          showsIndicators: false
+        ) {
+          HStack {
+            ForEach(0..<10) { int in
+              // MARK: Recommend Card
+              HStack {
+                Image(systemName: "person.circle.fill")
+                  .resizable()
+                  .frame(width: 64, height: 64)
+                
+                VStack(alignment: .leading) {
+                  GSText(
+                    style: .title3,
+                    string: "@RandomBrazilGuy"
+                  )
+                  .fontWeight(.semibold)
+                  
+                  HStack {
+                    GSText(style: .body1, string: "1.9k followers")
+                    
+                    GSText(style: .body1, string: "1.9k Repos")
+                  }
+                  
+                  GSButton(
+                    style: .secondary(
+                      ableState: .enabled
+                    )
+                  ) {
+                    print()
+                  } label: {
+                    Text("Knock!")
+                  }
+                  .frame(
+                    maxWidth: .infinity,
+                    alignment: .trailing
+                  )
+                }
+              }
+              .padding(.vertical, 14)
+              .padding(.horizontal, 20)
+              .background {
+                RoundedRectangle(cornerRadius: 35)
+                  .foregroundStyle(Color.white)
+                  .shadow(
+                    color: Color.gsGray1,
+                    radius: 8,
+                    x: 0,
+                    y: 2
+                  )
+              }
+            }
+            .padding(.vertical, 15)
+            .padding(.horizontal, 10)
+          }
+        }
+      } header: {
+        GSText(
+          style: .body2,
+          string: "Recommended “pals” for you 👋"
+        )
+        .frame(
+          maxWidth: .infinity,
+          alignment: .leading
+        )
+        .padding(.horizontal, 20)
+      }
+      
+      Divider()
+      
+      Section {
+        ScrollView(
+          .vertical,
+          showsIndicators: false
+        ) {
+          ForEach(0..<10) { int in
+            NavigationLink {
+              ChatDetailView()
+            } label: {
+              VStack(alignment: .leading) {
+                HStack(spacing: 20) {
+                  // MARK: LOFI
+                  Image(systemName: "person.circle.fill")
+                    .resizable()
+                    .frame(width: 52, height: 52)
+                    .foregroundStyle(Color.black)
+                  
+                  VStack(
+                    alignment: .leading,
+                    spacing: 6
+                  ) {
+                    GSText(
+                      style: .title3,
+                      string: "@RandomBrazilGirl"
+                    )
+                    .fontWeight(.semibold)
+                    
                     HStack {
-                        ForEach(0..<10) { int in
-                            // MARK: Recommend Card
-                            HStack {
-                                Image(systemName: "person.circle.fill")
-                                    .resizable()
-                                    .frame(width: 64, height: 64)
-                                
-                                VStack(alignment: .leading) {
-                                    GSText(
-                                        style: .title3,
-                                        string: "@RandomBrazilGuy"
-                                    )
-                                    .fontWeight(.semibold)
-                                    
-                                    HStack {
-                                        GSText(style: .body1, string: "1.9k followers")
-                                        
-                                        GSText(style: .body1, string: "1.9k Repos")
-                                    }
-                                    
-                                    GSButton(
-                                        style: .secondary(
-                                            ableState: .enabled
-                                        )
-                                    ) {
-                                        print()
-                                    } label: {
-                                        Text("Knock!")
-                                    }
-                                    .frame(
-                                        maxWidth: .infinity,
-                                        alignment: .trailing
-                                    )
-                                }
-                            }
-                            .padding(.vertical, 14)
-                            .padding(.horizontal, 20)
-                            .background {
-                                RoundedRectangle(cornerRadius: 35)
-                                    .foregroundStyle(Color.white)
-                                    .shadow(
-                                        color: Color.gsGray1,
-                                        radius: 8,
-                                        x: 0,
-                                        y: 2
-                                    )
-                            }
-                        }
-                        .padding(.vertical, 15)
-                        .padding(.horizontal, 10)
+                      GSText(
+                        style: .body2,
+                        string: "Hi I am RBG"
+                      )
+                      .fontWeight(.semibold)
+                      
+                      Spacer()
+                      
+                      ZStack {
+                        Circle()
+                          .frame(width: 20)
+                          .foregroundStyle(Color.gsGreenPrimary)
+                        
+                        GSText(style: .caption1, string: "3")
+                      }
                     }
+                  }
                 }
-            } header: {
-                GSText(
-                    style: .body2,
-                    string: "Recommended “pals” for you 👋"
-                )
-                .frame(
-                    maxWidth: .infinity,
-                    alignment: .leading
-                )
-                .padding(.horizontal, 20)
+                
+                Divider()
+              }
             }
-            
-            Divider()
-            
-            Section {
-                ScrollView(
-                    .vertical,
-                    showsIndicators: false
-                ) {
-                    ForEach(0..<10) { int in
-                        VStack(alignment: .leading) {
-                            HStack(spacing: 20) {
-                                Image(systemName: "person.circle.fill")
-                                    .resizable()
-                                    .frame(width: 52, height: 52)
-                                
-                                VStack(
-                                    alignment: .leading,
-                                    spacing: 6
-                                ) {
-                                    GSText(
-                                        style: .title3,
-                                        string: "Random Brazil Girl"
-                                    )
-                                    .fontWeight(.semibold)
-                                    
-                                    HStack {
-                                        GSText(
-                                            style: .body2,
-                                            string: "@randombrazilgirl"
-                                        )
-                                        .fontWeight(.semibold)
-                                        
-                                        Spacer()
-                                        
-                                        ZStack {
-                                            Circle()
-                                                .frame(width: 20)
-                                                .foregroundStyle(Color.gsGreenPrimary)
-                                            
-                                            Text("3")
-                                        }
-                                    }
-                                }
-                            }
-                            
-                            Divider()
-                        }
-                    }
-                }
-            } header: {
-                GSText(
-                    style: .body2,
-                    string: "My Penpals"
-                )
-                .frame(
-                    maxWidth: .infinity,
-                    alignment: .leading
-                )
-            }
-            .padding(.horizontal, 20)
-            .padding(.top, 12)
+          }
         }
-        .toolbar {
-            // MARK: All LOFI
-            ToolbarItem(placement: .topBarLeading) {
-                Text("GITSPACE")
-            }
-            
-            ToolbarItem(placement: .topBarTrailing) {
-                Button {
-                    print()
-                } label: {
-                    Image(systemName: "archivebox.fill")
-                }
-            }
-             
-            ToolbarItem(placement: .topBarTrailing) {
-                Button {
-                    print()
-                } label: {
-                    Image(systemName: "plus.bubble.fill")
-                }
-            }
-        }
+      } header: {
+        GSText(
+          style: .body2,
+          string: "My Penpals"
+        )
+        .frame(
+          maxWidth: .infinity,
+          alignment: .leading
+        )
+      }
+      .padding(.horizontal, 20)
+      .padding(.top, 12)
     }
+    .toolbar {
+      // MARK: All LOFI
+      ToolbarItem(placement: .topBarLeading) {
+        Text("GITSPACE")
+      }
+      
+      ToolbarItem(placement: .topBarTrailing) {
+        Button {
+          print()
+          
+        } label: {
+          Image(systemName: "archivebox.fill")
+        }
+        .foregroundStyle(Color.gsGray2)
+      }
+      
+      ToolbarItem(placement: .topBarTrailing) {
+        Button {
+          print()
+        } label: {
+          Image(systemName: "plus.bubble.fill")
+        }
+        .foregroundStyle(Color.gsGray2)
+      }
+    }
+  }
 }
 
 #Preview {
-    NavigationStack {
-        ChatListView()
-    }
+  NavigationStack {
+    ChatListView()
+  }
 }
