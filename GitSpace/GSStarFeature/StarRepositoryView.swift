86564243c66ab7633b2eea221bd0acb90a19da86
dahae0320 <ldh990320ldh@gmail.com>
2024-01-27 14:03:42 +0900

    [Add] #22 GSStarFeature 프레임워크 추가

---
+++ GitSpace/GSStarFeature/StarRepositoryView.swift
@@ -0,0 +1,18 @@
+//
+//  StarRepositoryView.swift
+//  GSStarFeature
+//
+//  Created by Da Hae Lee on 1/27/24.
+//
+
+import SwiftUI
+
+struct StarRepositoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StarRepositoryView()
+}
