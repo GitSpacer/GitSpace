4378f8fc4b01d1696697cc72c4ae73750de43a47
dahae0320 <ldh990320ldh@gmail.com>
2024-01-28 16:26:54 +0900

    [Add] #22 TaggingView 추가

---
+++ GitSpace/GSStarFeature/TaggingView.swift
@@ -0,0 +1,20 @@
+//
+//  TaggingView.swift
+//  GSStarFeature
+//
+//  Created by Da Hae Lee on 1/28/24.
+//
+
+import SwiftUI
+
+struct TaggingView: View {
+    var body: some View {
+        // TODO: Tag CRUD를 위한 View 구현
+        /// 구 AddNewTagsView
+        Text("Tag: create, read, update, delete")
+    }
+}
+
+#Preview {
+    TaggingView()
+}
