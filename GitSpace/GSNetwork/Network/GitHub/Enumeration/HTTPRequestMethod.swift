97aa93c63ea38f6215cd0884a1576728324602e5
jegyun <parkjekyun@gmail.com>
2024-02-14 00:23:01 +0900

    [Add] #24 HTTP Request Method 열거형 추가

---
+++ GitSpace/GSNetwork/Network/GitHub/Enumeration/HTTPRequestMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPRequestMethod.swift
+//  GSNetwork
+//
+//  Created by 박제균 on 2/14/24.
+//
+
+import Foundation
+
+enum HTTPRequestMethod: String {
+  case get = "GET"
+  case post = "POST"
+  case put = "PUT"
+  case delete = "DELETE"
+}
