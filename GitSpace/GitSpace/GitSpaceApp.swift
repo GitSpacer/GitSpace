8cae1f745113410cbe959d2ee392de7ed982db4d
Taeyoung Won <hexter1994@gmail.com>
2024-02-27 14:52:23 +0900

    [Clean] #21 콜론 린트 반영

--- GitSpace/GitSpace/GitSpaceApp.swift
+++ GitSpace/GitSpace/GitSpaceApp.swift
@@ -10,7 +10,7 @@ import FirebaseCore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
     FirebaseApp.configure()
     
     return true
