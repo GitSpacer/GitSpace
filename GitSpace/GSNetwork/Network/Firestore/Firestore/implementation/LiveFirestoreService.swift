7acef41344c18e50875fcbf5286c03cafd67d876
Taeyoung Won <hexter1994@gmail.com>
2024-03-13 16:30:09 +0900

    [Feat] #21 서브컬렉션 CRUD 메서드 구현

--- GitSpace/GSNetwork/Network/Firestore/Firestore/implementation/LiveFirestoreService.swift
+++ GitSpace/GSNetwork/Network/Firestore/Firestore/implementation/LiveFirestoreService.swift
@@ -15,6 +15,37 @@ public final class LiveFirestoreService: FirestoreService {
 // MARK: - Interface Method
 extension LiveFirestoreService {
   
+  /// 지정한 컬렉션의 경로를 반환합니다.
+  /// - Parameter from collection: 경로를 검색할 Firestore 컬렉션입니다.
+  /// - Returns: 지정된 컬렉션에 대한 CollectionReference를 반환합니다.
+  /// - Author: 원태영
+  public func getCollectionPath(from collection: FirestoreCollection) -> CollectionReference {
+    
+    return firestore
+      .collection(collection.name)
+  }
+  
+  
+  /// 지정한 컬렉션의 하위 컬렉션 경로를 반환합니다.
+  /// - Parameters:
+  ///   - superCol superCollection: 상위 컬렉션입니다.
+  ///   - superDoc superDocumentID: 상위 문서의 ID입니다.
+  ///   - from subCollection: 하위 컬렉션입니다.
+  /// - Returns: 하위 컬렉션에 대한 CollectionReference를 반환합니다.
+  /// - Author: 원태영
+  public func getCollectionPath(
+    superCol superCollection: FirestoreCollection,
+    superDoc superDocumentID: String,
+    from subCollection: FirestoreCollection
+  ) -> CollectionReference {
+    
+    return firestore
+      .collection(superCollection.name)
+      .document(superDocumentID)
+      .collection(subCollection.name)
+  }
+  
+  
   /// 지정된 컬렉션에 모델을 생성합니다.
   /// - Parameters:
   ///   - in collection: 모델을 생성할 컬렉션입니다.
@@ -32,6 +63,54 @@ extension LiveFirestoreService {
     let docRef: DocumentReference = getDocumentPath(from: collection, at: docID)
     
     try addDocument(at: docRef, with: model)
+  }
+  
+  
+  /// 지정된 컬렉션에 모델을 생성합니다.
+  /// - Parameters:
+  ///   - superCol superCollection: 상위 컬렉션입니다.
+  ///   - superDoc superDocumentID: 상위 문서의 ID입니다.
+  ///   - in collection: 모델을 생성할 컬렉션입니다.
+  ///   - with model: 저장할 모델의 인스턴스입니다.
+  /// - Author: 원태영
+  public func create<T: GSModel>(
+    superCol superCollection: FirestoreCollection,
+    superDoc superDocumentID: String,
+    in collection: FirestoreCollection,
+    with model: T
+  ) throws {
+    
+    guard let docID = model.id as? String else {
+      throw GSModelError.castIDFailed(from: #function)
+    }
+    
+    let docRef: DocumentReference = getDocumentPath(
+      superCol: superCollection,
+      superDoc: superDocumentID,
+      from: collection,
+      at: docID
+    )
+    
+    try addDocument(at: docRef, with: model)
+  }
+  
+  
+  /// 지정된 컬렉션의 모든 문서를 조회합니다.
+  /// - Parameters:
+  ///   - from collection: 조회할 컬렉션입니다.
+  /// - Returns: 해당 컬렉션의 모든 모델을 포함하는 배열을 반환합니다.
+  /// - Author: 원태영
+  public func fetch<T: GSModel>(
+    from collection: FirestoreCollection
+  ) async throws -> [T] {
+    
+    let colRef: CollectionReference = getCollectionPath(from: collection)
+    
+    let documents: [QueryDocumentSnapshot] = try await getDocuments(from: colRef)
+    
+    let modelArray: [T] = try documents.map { try $0.data(as: T.self) }
+    
+    return modelArray
   }
   
   
@@ -58,14 +137,22 @@ extension LiveFirestoreService {
   
   /// 지정된 컬렉션의 모든 문서를 조회합니다.
   /// - Parameters:
+  ///   - superCol superCollection: 상위 컬렉션입니다.
+  ///   - superDoc superDocumentID: 상위 문서의 ID입니다.
   ///   - from collection: 조회할 컬렉션입니다.
   /// - Returns: 해당 컬렉션의 모든 모델을 포함하는 배열을 반환합니다.
   /// - Author: 원태영
   public func fetch<T: GSModel>(
+    superCol superCollection: FirestoreCollection,
+    superDoc superDocumentID: String,
     from collection: FirestoreCollection
   ) async throws -> [T] {
     
-    let colRef: CollectionReference = getCollectionPath(from: collection)
+    let colRef: CollectionReference = getCollectionPath(
+      superCol: superCollection,
+      superDoc: superDocumentID,
+      from: collection
+    )
     
     let documents: [QueryDocumentSnapshot] = try await getDocuments(from: colRef)
     
@@ -75,20 +162,92 @@ extension LiveFirestoreService {
   }
   
   
+  /// 지정된 컬렉션과 문서 ID를 사용하여 모델을 조회합니다.
+  /// - Parameters:
+  ///   - superCol superCollection: 상위 컬렉션입니다.
+  ///   - superDoc superDocumentID: 상위 문서의 ID입니다.
+  ///   - from collection: 조회할 컬렉션입니다.
+  ///   - at documentID: 조회할 문서의 ID입니다.
+  /// - Returns: 지정된 타입의 모델을 반환합니다.
+  /// - Author: 원태영
+  public func fetch<T: GSModel>(
+    superCol superCollection: FirestoreCollection,
+    superDoc superDocumentID: String,
+    from collection: FirestoreCollection,
+    at documentID: String
+  ) async throws -> T {
+    
+    let docRef: DocumentReference = getDocumentPath(
+      superCol: superCollection,
+      superDoc: superDocumentID,
+      from: collection,
+      at: documentID
+    )
+    
+    let document: DocumentSnapshot = try await getDocument(at: docRef)
+    
+    let model: T = try document.data(as: T.self)
+    
+    return model
+  }
+  
+  
   /// 지정된 컬렉션에서 특정 조건을 만족하는 모든 문서를 조회합니다.
   /// - Parameters:
   ///   - from colRef: 조회할 컬렉션입니다.
-  ///   - where query: 검사할 필드와 조건 오퍼레이션이 포함된 튜플 배열입니다.
+  ///   - where field: 조건을 적용할 필드입니다.
+  ///   - by query: 조건으로 적용할 쿼리 연산자입니다.
   /// - Returns: 조건을 만족하는 모든 모델을 포함하는 배열을 반환합니다.
   /// - Author: 원태영
   public func fetch<T: GSModel>(
     from collection: FirestoreCollection,
-    where query: WhereField
+    where field: any FirestoreFieldProtocol,
+    by query: FirestoreQueryOperation
   ) async throws -> [T] {
     
     let colRef: CollectionReference = getCollectionPath(from: collection)
     
-    let documents: [QueryDocumentSnapshot] = try await getDocuments(from: colRef, where: query)
+    let documents: [QueryDocumentSnapshot] = try await getQueryDocuments(
+      from: colRef,
+      where: field,
+      by: query
+    )
+    
+    let modelArray: [T] = try documents.map { try $0.data(as: T.self) }
+    
+    return modelArray
+  }
+  
+  
+  /// 지정된 컬렉션에서 특정 조건을 만족하는 모든 문서를 조회합니다.
+  /// - 단일 쿼리일 때 사용합니다. 복합 쿼리를 사용하시려면 getCollectionPath로 컬렉션 경로를 설정하고, .query 체이닝으로 원하는 조건을 설정한 뒤 .fetch를 호출해주세요.
+  /// - Parameters:
+  ///   - superCol superCollection: 상위 컬렉션입니다.
+  ///   - superDoc superDocumentID: 상위 문서의 ID입니다.
+  ///   - from colRef: 조회할 컬렉션입니다.
+  ///   - where field: 조건을 적용할 필드입니다.
+  ///   - by query: 조건으로 적용할 쿼리 연산자입니다.
+  /// - Returns: 조건을 만족하는 모든 모델을 포함하는 배열을 반환합니다.
+  /// - Author: 원태영
+  public func fetch<T: GSModel>(
+    superCol superCollection: FirestoreCollection,
+    superDoc superDocumentID: String,
+    from collection: FirestoreCollection,
+    where field: any FirestoreFieldProtocol,
+    by query: FirestoreQueryOperation
+  ) async throws -> [T] {
+    
+    let colRef: CollectionReference = getCollectionPath(
+      superCol: superCollection,
+      superDoc: superDocumentID,
+      from: collection
+    )
+    
+    let documents: [QueryDocumentSnapshot] = try await getQueryDocuments(
+      from: colRef,
+      where: field,
+      by: query
+    )
     
     let modelArray: [T] = try documents.map { try $0.data(as: T.self) }
     
@@ -120,6 +279,39 @@ extension LiveFirestoreService {
   }
   
   
+  /// 지정된 컬렉션의 모델을 업데이트합니다. 모델의 ID를 사용해서 문서를 조회합니다.
+  /// - Parameters:
+  ///   - superCol superCollection: 상위 컬렉션입니다.
+  ///   - superDoc superDocumentID: 상위 문서의 ID입니다.
+  ///   - in collection: 업데이트 모델이 위치한 컬렉션입니다.
+  ///   - at model: 업데이트할 모델입니다.
+  ///   - updating fields: 업데이트할 필드 리스트입니다.
+  /// - Author: 원태영
+  public func update<T: GSModel, U: FirestoreFieldProtocol>(
+    superCol superCollection: FirestoreCollection,
+    superDoc superDocumentID: String,
+    in collection: FirestoreCollection,
+    at model: T,
+    updating fields: [U]
+  ) throws {
+    
+    guard let docID = model.id as? String else {
+      throw GSModelError.castIDFailed(from: #function)
+    }
+    
+    let docRef: DocumentReference = getDocumentPath(
+      superCol: superCollection,
+      superDoc: superDocumentID,
+      from: collection,
+      at: docID
+    )
+    
+    let updatingFields: [String: Any] = makeFields(with: fields, to: model)
+    
+    updateDocument(at: docRef, with: updatingFields)
+  }
+  
+  
   /// 지정된 컬렉션의 문서를 찾아서 삭제합니다.
   /// - Parameters:
   ///   - in collection: 삭제할 컬렉션입니다.
@@ -134,24 +326,36 @@ extension LiveFirestoreService {
     
     deleteDocument(at: docRef)
   }
+  
+  /// 지정된 컬렉션의 문서를 찾아서 삭제합니다.
+  /// - Parameters:
+  ///   - superCol superCollection: 상위 컬렉션입니다.
+  ///   - superDoc superDocumentID: 상위 문서의 ID입니다.
+  ///   - in collection: 삭제할 컬렉션입니다.
+  ///   - at documentID: 삭제할 문서의 ID입니다.
+  /// - Author: 원태영
+  public func delete(
+    superCol superCollection: FirestoreCollection,
+    superDoc superDocumentID: String,
+    in collection: FirestoreCollection,
+    at documentID: String
+  ) {
+    
+    let docRef: DocumentReference = getDocumentPath(
+      superCol: superCollection,
+      superDoc: superDocumentID,
+      from: collection,
+      at: documentID
+    )
+    
+    deleteDocument(at: docRef)
+  }
 }
 
 
 // MARK: - Private
-
 // MARK: - Get Path
 extension LiveFirestoreService {
-  
-  /// 지정한 컬렉션의 경로를 반환합니다.
-  /// - Parameter from collection: 경로를 검색할 Firestore 컬렉션입니다.
-  /// - Returns: 지정된 컬렉션에 대한 CollectionReference를 반환합니다.
-  /// - Author: 원태영
-  public func getCollectionPath(from collection: FirestoreCollection) -> CollectionReference {
-    
-    return firestore
-      .collection(collection.name)
-  }
-  
   
   /// 지정한 컬렉션에서 단일 문서 경로를 반환합니다.
   /// - Parameters:
@@ -167,26 +371,6 @@ extension LiveFirestoreService {
     return firestore
       .collection(collection.name)
       .document(documentID)
-  }
-  
-  
-  /// 지정한 컬렉션의 하위 컬렉션 경로를 반환합니다.
-  /// - Parameters:
-  ///   - superCol superCollection: 상위 컬렉션입니다.
-  ///   - superDoc superDocumentID: 상위 문서의 ID입니다.
-  ///   - from subCollection: 하위 컬렉션입니다.
-  /// - Returns: 하위 컬렉션에 대한 CollectionReference를 반환합니다.
-  /// - Author: 원태영
-  public func getCollectionPath(
-    superCol superCollection: FirestoreCollection,
-    superDoc superDocumentID: String,
-    from subCollection: FirestoreCollection
-  ) -> CollectionReference {
-    
-    return firestore
-      .collection(superCollection.name)
-      .document(superDocumentID)
-      .collection(subCollection.name)
   }
   
   
@@ -320,16 +504,18 @@ extension LiveFirestoreService {
   /// 지정한 경로에서 조건을 만족하는 모든 문서를 조회합니다.
   /// - Parameters:
   ///   - from colRef: 조회할 컬렉션의 경로입니다.
-  ///   - where query: 검사할 필드와 조건 쿼리가 합쳐진 튜플입니다.
+  ///   - where field: 조건을 적용할 필드입니다.
+  ///   - by query: 조건으로 적용할 쿼리 연산자입니다.
   /// - Returns: 조건을 만족하는 문서들의 배열을 반환합니다.
   /// - Throws: 쿼리 결과가 비어있을 때 오류를 방출합니다.
   /// - Author: 원태영
-  private func getDocuments(
+  private func getQueryDocuments(
     from colRef: CollectionReference,
-    where query: WhereField
+    where field: any FirestoreFieldProtocol,
+    by query: FirestoreQueryOperation
   ) async throws -> [QueryDocumentSnapshot] {
     
-    let query = makeQuery(from: colRef, where: query.field, operation: query.operation)
+    let query = makeQuery(from: colRef, where: field, operation: query)
     
     let snapshot: QuerySnapshot = try await query.getDocuments()
     
