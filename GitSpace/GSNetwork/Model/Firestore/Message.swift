6cc7229ad7d0529eca591e60c94578e79a4c25da
Taeyoung Won <hexter1994@gmail.com>
2024-03-13 16:29:32 +0900

    [Add] #21 더미 메세지 모델 추가

--- GitSpace/GSNetwork/Model/Firestore/Message.swift
+++ GitSpace/GSNetwork/Model/Firestore/Message.swift
@@ -8,4 +8,14 @@ public struct Message: GSModel {
   public var imageContent: String? // 메세지에 첨부한 이미지
   public var sentDate: Date        // 메세지 작성 날짜
   public var isRead: Bool          // 수신 유저의 메세지 확인 여부
+  
+  public static var dummy: Message {
+    return Message(
+      id: UUID().uuidString,
+      senderID: "",
+      textContent: "메세지 내용입니다.",
+      sentDate: .now,
+      isRead: false
+    )
+  }
 }
